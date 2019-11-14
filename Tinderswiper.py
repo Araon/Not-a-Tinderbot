08f1b5af8ceecd0317c7233edc73fd01f0d5f113
Soumik Ghosh <soumik.ghosh1998@gmail.com>
2019-11-14 21:17:36 +0530

    Version 2
    Added random feature

--- Tinderswiper.py
+++ Tinderswiper.py
@@ -1,14 +1,19 @@
 import pyautogui
 import webbrowser
 import time
+import random
 
-input("This program will open up Tinder and do the right swiping for you\nPress enter to start Program")
+print("****Tinder swiper V2****")
+
+
 a=int(input('How many people do you want to swipe?\n'))
 webbrowser.open('https://www.tinder.com',new = 1,autoraise = True)
 time.sleep(5)
-i=0
-while i<a:
-    pyautogui.press('right')
-    i=i+1
-    time.sleep(1)
-    print('swiped person number',i)
+
+for i in range(0,a) :
+	if random.randint(0,1) > 0.5:
+		pyautogui.press('right')
+	else:
+		pyautogui.press('left')
+	time.sleep(1)
+	print('person',i+1,'swiped')
